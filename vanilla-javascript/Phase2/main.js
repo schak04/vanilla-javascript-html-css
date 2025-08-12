2c606ff658e4d31664f034c2cb95651fe5d030ed
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-08-12 10:28:34 +0530

    Add notes for the concept of shuffling an array

--- vanilla-javascript/Phase2/main.js
+++ vanilla-javascript/Phase2/main.js
@@ -1067,3 +1067,26 @@ function optionalChainingDemo() {
 function templateLiteralDemo(name = "Guest") {
     print(`Welcome, ${name}! Today is ${new Date().toLocaleDateString()}`);
 }
+
+/* Shuffling an Array (for making small card games or other games that require shuffling):
+    Shuffling an array can be done using the Fisher–Yates algorithm.
+    The Fisher-Yates shuffle is an algorithm for shuffling a finite sequence.
+    The algorithm takes a list of all the elements of the sequence, and continually
+    determines the next element in the shuffled sequence by randomly drawing an element
+    from the list until no elements remain. The algorithm produces an unbiased
+    permutation: every permutation is equally likely.
+    The modern version of the algorithm takes time proportional to the number of items
+    being shuffled and shuffles them in place.
+
+Here's an example:
+=================
+const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+shuffle(cards);
+console.log(cards);
+function shuffle(array){
+    for(let i = array.length - 1; i > 0; i--){
+        const random = Math.floor(Math.random() * (i + 1));
+        [array[i], array[random]] = [array[random], array[i]];
+    }
+}
+*/
