d7a48f4b36ffdfc2751d87dfd754993a606160cd
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-10-06 00:15:36 +0530

    Add notes and examples for cookies

--- vanilla-javascript/Phase2/main.js
+++ vanilla-javascript/Phase2/main.js
@@ -1855,3 +1855,44 @@ fetch("gamess.json")
     .then(response => response.json()) /* response.json() also returns a Promise */
     .then(values => values.forEach(value => console.log(value)))
     .catch(error => console.error(error));
+
+/* Cookie: A small text file stored on a computer used to remember information about the user saved in name=value pairs. */
+/* manual way */
+// console.log(navigator.cookieEnabled); // -> to check if cookies are enabled
+// document.cookie = "firstName=ABC; expires=Sun, 1 January 2030 12:00:00 UTC; path=/"; // -> document.cookie adds a cookie
+// document.cookie = "lastName=XYZ; expires=Sun, 1 January 2030 12:00:00 UTC; path=/";
+// let allCookies = document.cookie;
+// console.dir(allCookies);
+/* non-manual way */
+const firstText = document.querySelector("#firstText");
+const lastText = document.querySelector("#lastText");
+const submitBtn = document.querySelector("#submitBtn");
+const cookieBtn = document.querySelector("#cookieBtn");
+submitBtn.addEventListener("click", () => {
+    setCookie("firstName", firstText.value, 365);
+    setCookie("lastName", lastText.value, 365);
+});
+cookieBtn.addEventListener("click", () => {
+    firstText.value = getCookie("firstName");
+    lastText.value = getCookie("lastName");
+});
+function setCookie(name, value, daysToLive) {
+    const date = new Date();
+    date.setTime(date.getTime() + (daysToLive * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}=${value}; ${expires}; path=/`
+}
+function deleteCookie(name) {
+    setCookie(name, null, null);
+}
+function getCookie(name) {
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArray = cDecoded.split("; ");
+    let result = null;
+    cArray.forEach(element => {
+        if (element.indexOf(name) == 0) {
+            result = element.substring(name.length + 1)
+        }
+    })
+    return result;
+}
